eb6964a96c84ff527ef7f55acb66aa76fe5fdbdb
nikmanchan <nikmanchan@gmail.com>
2023-03-17 00:41:03 -0500

    add tensorflow and print tensor on click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
 
 function App() {
   const handleRunTraining = (e) => {
     e.preventDefault();
     console.log('Run training');
+    tf.tensor([1, 2, 3, 4], [2, 2]).print();
   }
   return (
     <div className="App">
